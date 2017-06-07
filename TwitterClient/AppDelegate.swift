239723179f717acf57166e10f54751dca290cc93
Cole Dunsby <coledunsby@gmail.com>
2017-06-06 23:08:39 -0400

    uiappearance

--- TwitterClient/AppDelegate.swift
+++ TwitterClient/AppDelegate.swift
@@ -14,7 +14,36 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        customizeAppearance()
         return true
+    }
+    
+    private func customizeAppearance() {
+        let primaryColor = #colorLiteral(red: 0.1137254902, green: 0.631372549, blue: 0.9490196078, alpha: 1)
+        let secondaryColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        
+        UINavigationBar.appearance().titleTextAttributes = [
+            NSFontAttributeName: UIFont(name: "AvenirNext-DemiBold", size: 18)!,
+            NSForegroundColorAttributeName: secondaryColor
+        ]
+        
+        UINavigationBar.appearance().isTranslucent = false
+        UINavigationBar.appearance().tintColor = secondaryColor
+        UINavigationBar.appearance().barTintColor = primaryColor
+        
+        UITabBar.appearance().isTranslucent = false
+        UITabBar.appearance().tintColor = secondaryColor
+        UITabBar.appearance().barTintColor = primaryColor
+        
+        UIBarButtonItem.appearance().setTitleTextAttributes([
+            NSFontAttributeName: UIFont(name: "AvenirNext-DemiBold", size: 16)!,
+            NSForegroundColorAttributeName: secondaryColor
+        ], for: .normal)
+        
+        UITabBarItem.appearance().setTitleTextAttributes([
+            NSFontAttributeName: UIFont(name: "AvenirNext-Medium", size: 9)!,
+            NSForegroundColorAttributeName: secondaryColor
+        ], for: .normal)
     }
 }
 
